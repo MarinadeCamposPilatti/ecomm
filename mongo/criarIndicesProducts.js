9cab22b8a27d840373580452dc3ac3ab8f6d6a9f
Marina Pilatti <marina.campospilatti03@gmail.com>
2023-06-02 17:20:56 -0300

    criar indices de produtos

---
+++ mongo/criarIndicesProducts.js
@@ -0,0 +1,7 @@
+use("ecomm");
+
+const IndicesdosNomes = db.products.createIndex({"NOME": 1});
+const IndicesdasCategorias = db.products.createIndex({"CATEGORIA": 1});
+const IndicesdosPrecos = db.products.createIndex({"PREÇO UNITÁRIO": -1});
+
+console.log(IndicesdosNomes, "\n",IndicesdasCategorias, "\n",IndicesdosPrecos);
